ee4c9fb407d109bddd30a393e854560c88004144
Rodrigo Cuervo <rodrigo_vigil@yahoo.com.br>
2022-06-21 18:54:20 -0300

    Exercicio 3 - correção

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Title extends Component {
   render() {
     const { headline } = this.props;
-    return (
-      <div>
-        <h2>{headline}</h2>
-      </div>
+    console.log(headline);
+    return (<h2>{headline}</h2>
     );
   }
 }
+
 Title.propTypes = {
   headline: PropTypes.string.isRequired,
 };
